9dbf96353f9dd474174ebe003b15b7d0d29b3358
Michael Whittaker <mjwhittaker@berkeley.edu>
2018-08-25 21:59:24 -0700

    Colored logging.

--- shared/src/main/scala/zeno/JsLogger.scala
+++ shared/src/main/scala/zeno/JsLogger.scala
@@ -5,30 +5,42 @@ import scala.scalajs.js
 import scala.scalajs.js.JSConverters._
 import scala.scalajs.js.annotation._;
 
-// TODO(mwhittaker): Allow javascript to add custom colors.
+@JSExportAll
+object JsLogEntryType extends Enumeration {
+  val Debug = Value("DEBUG")
+  val Info = Value("INFO")
+  val Warn = Value("WARN")
+  val Error = Value("ERROR")
+  val Fatal = Value("FATAL")
+}
+
+@JSExportAll
+case class JsLogEntry(typ: JsLogEntryType.Value, entry: String)
+
 @JSExportAll
 class JsLogger extends Logger {
-  private val bufferedLogs = Buffer[String]()
-  def bufferedLogsJs(): js.Array[String] = { bufferedLogs.toJSArray }
+  private val bufferedLogs = Buffer[JsLogEntry]()
+  def bufferedLogsJs(): js.Array[JsLogEntry] = { bufferedLogs.toJSArray }
   def clearBufferedLogs(): Unit = { bufferedLogs.clear() }
 
   override def fatal(message: String): Unit = {
-    bufferedLogs += s"[FATAL] $message"
+    // TODO(mwhittaker): Crash program.
+    bufferedLogs += JsLogEntry(JsLogEntryType.Fatal, s"$message")
   }
 
   override def error(message: String): Unit = {
-    bufferedLogs += s"[ERROR] $message"
+    bufferedLogs += JsLogEntry(JsLogEntryType.Error, s"$message")
   }
 
   override def warn(message: String): Unit = {
-    bufferedLogs += s"[WARN ] $message"
+    bufferedLogs += JsLogEntry(JsLogEntryType.Warn, s"$message")
   }
 
   override def info(message: String): Unit = {
-    bufferedLogs += s"[INFO ] $message"
+    bufferedLogs += JsLogEntry(JsLogEntryType.Info, s"$message")
   }
 
   override def debug(message: String): Unit = {
-    bufferedLogs += s"[DEBUG] $message"
+    bufferedLogs += JsLogEntry(JsLogEntryType.Debug, s"$message")
   }
 }
