e5a6d52e441459d417db63fd9b6b13c4440ba193
Michael Whittaker <mjwhittaker@berkeley.edu>
2018-08-25 20:04:15 -0700

    Added node coloring + click to choose.

--- js/src/main/js/examples/echo.js
+++ js/src/main/js/examples/echo.js
@@ -13,37 +13,52 @@ function main() {
   let nodes = {};
   nodes[Echo.server.address] = {
     actor: Echo.server,
-    svg: snap.circle(150, 50, 15),
+    svg: snap.circle(150, 50, 20).attr(
+        {fill: '#e74c3c', stroke: 'black', 'stroke-width': '3pt'}),
     vue_node: new VueNode('Server'),
   }
   nodes[Echo.clientA.address] = {
     actor: Echo.clientA,
-    svg: snap.circle(100, 150, 15),
+    svg: snap.circle(75, 150, 20).attr(
+        {fill: '#3498db', stroke: 'black', 'stroke-width': '3pt'}),
     vue_node: new VueNode('Client A'),
   }
   nodes[Echo.clientB.address] = {
     actor: Echo.clientB,
-    svg: snap.circle(200, 150, 15),
+    svg: snap.circle(225, 150, 20).attr(
+        {fill: '#2ecc71', stroke: 'black', 'stroke-width': '3pt'}),
     vue_node: new VueNode('Client B'),
   }
 
+  // Add node titles.
+  snap.text(150, 20, 'Server').attr({'text-anchor': 'middle'});
+  snap.text(75, 190, 'Client A').attr({'text-anchor': 'middle'});
+  snap.text(225, 190, 'Client B').attr({'text-anchor': 'middle'});
+
   let vue_app = new Vue({
     el: '#app',
-    data: { nodes: Object.values(nodes) },
+    data: { vue_node: nodes[Echo.server.address].vue_node },
   });
+
+  // Select a node by clicking it.
+  for (let node of Object.values(nodes)) {
+    node.svg.node.onclick = () => {
+      vue_app.vue_node = node.vue_node;
+    }
+  }
 
   let simulated_app = new zenojs.SimulatedApp(Echo.transport, {
     config_message: (app, message) => {
       let src = nodes[message.src];
       let dst = nodes[message.dst];
       let svg_message = snap.circle(src.svg.attr("cx"), src.svg.attr("cy"), 9)
-                            .attr({fill: 'red'});
+                            .attr({fill: '#2c3e50'});
       snap.prepend(svg_message);
       return {
         svg_message: svg_message,
         animate_args: {cx: dst.svg.attr("cx"), cy: dst.svg.attr("cy")},
         timeout: 250 + Math.random() * 200,
-        drop: Math.random() <= 0.1,
+        drop: Math.random() <= 0.01,
       };
     },
     on_send: (app, message) => {},
