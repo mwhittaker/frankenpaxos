276255e2005d4d376a7d684cda2e781ca9a78ca6
Michael Whittaker <mjwhittaker@berkeley.edu>
2018-11-13 20:00:25 -0800

    Tidied up echo js and html.

--- js/src/main/js/examples/echo.js
+++ js/src/main/js/examples/echo.js
@@ -1,4 +1,4 @@
-let client_echo_box = {
+let client_info = {
   props: ['node'],
 
   data: function() {
@@ -19,18 +19,25 @@ let client_echo_box = {
 
   template: `
     <div>
+      <div>Number of messages received: {{node.actor.numMessagesReceived}}</div>
       <button v-on:click="echo">Echo</button>
       <input v-model="message" v-on:keyup.enter="echo"></input>
     </div>
   `,
 };
 
+let server_info = {
+  props: ['node'],
 
-function simulated_app() {
-  let Echo = zeno.examples.js.SimulatedEcho.Echo;
-  let snap = Snap('#simulated_animation');
+  template: `
+    <div>
+      <div>Number of messages received: {{node.actor.numMessagesReceived}}</div>
+    </div>
+  `,
+};
 
-  // Create nodes.
+function make_nodes(Echo, snap) {
+  // Create the nodes.
   let nodes = {};
   nodes[Echo.server.address] = {
     actor: Echo.server,
@@ -53,9 +60,15 @@ function simulated_app() {
   snap.text(75, 190, 'Client A').attr({'text-anchor': 'middle'});
   snap.text(225, 190, 'Client B').attr({'text-anchor': 'middle'});
 
+  return nodes;
+}
+
+function make_app(Echo, snap, app_id) {
+  let nodes = make_nodes(Echo, snap)
+
   // Create the vue app.
   let vue_app = new Vue({
-    el: '#simulated_app',
+    el: app_id,
 
     data: {
       node: nodes[Echo.server.address],
@@ -76,73 +89,12 @@ function simulated_app() {
 
     computed: {
       current_component: function() {
-        if (this.node.actor != Echo.server) {
-          return client_echo_box;
-        }
-      },
-    },
-  });
-
-  // Select a node by clicking it.
-  for (let node of Object.values(nodes)) {
-    node.svg.node.onclick = () => {
-      vue_app.node = node;
-    }
-  }
-}
-
-function clickthrough_app() {
-  let Echo = zeno.examples.js.ClickthroughEcho.Echo;
-  let snap = Snap('#clickthrough_animation');
-
-  // Create nodes.
-  let nodes = {};
-  nodes[Echo.server.address] = {
-    actor: Echo.server,
-    svg: snap.circle(150, 50, 20).attr(
-        {fill: '#e74c3c', stroke: 'black', 'stroke-width': '3pt'}),
-  };
-  nodes[Echo.clientA.address] = {
-    actor: Echo.clientA,
-    svg: snap.circle(75, 150, 20).attr(
-        {fill: '#3498db', stroke: 'black', 'stroke-width': '3pt'}),
-  };
-  nodes[Echo.clientB.address] = {
-    actor: Echo.clientB,
-    svg: snap.circle(225, 150, 20).attr(
-        {fill: '#2ecc71', stroke: 'black', 'stroke-width': '3pt'}),
-  };
-
-  // Add node titles.
-  snap.text(150, 20, 'Server').attr({'text-anchor': 'middle'});
-  snap.text(75, 190, 'Client A').attr({'text-anchor': 'middle'});
-  snap.text(225, 190, 'Client B').attr({'text-anchor': 'middle'});
-
-  // Create the vue app.
-  let vue_app = new Vue({
-    el: '#clickthrough_app',
-
-    data: {
-      node: nodes[Echo.server.address],
-      transport: Echo.transport,
-      send_message: (message, callback) => {
-        let src = nodes[message.src];
-        let dst = nodes[message.dst];
-        let svg_message =
-          snap.circle(src.svg.attr("cx"), src.svg.attr("cy"), 9)
-              .attr({fill: '#2c3e50'});
-        snap.prepend(svg_message);
-        svg_message.animate(
-          {cx: dst.svg.attr("cx"), cy: dst.svg.attr("cy")},
-          200,
-          callback);
-      }
-    },
-
-    computed: {
-      current_component: function() {
-        if (this.node.actor != Echo.server) {
-          return client_echo_box;
+        if (this.node.actor.address.address.includes("Server")) {
+          return server_info;
+        } else if (this.node.actor.address.address.includes("Client")) {
+          return client_info;
+        } else {
+          // Impossible!
         }
       },
     },
@@ -157,8 +109,13 @@ function clickthrough_app() {
 }
 
 function main() {
-  simulated_app();
-  clickthrough_app();
+  make_app(zeno.examples.js.SimulatedEcho.Echo,
+           Snap('#simulated_animation'),
+           '#simulated_app');
+
+  make_app(zeno.examples.js.ClickthroughEcho.Echo,
+           Snap('#clickthrough_animation'),
+           '#clickthrough_app');
 }
 
 window.onload = main
