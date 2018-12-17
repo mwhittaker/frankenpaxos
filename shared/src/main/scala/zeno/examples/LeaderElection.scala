3d46cc4449be1e6144a20ab3b3d6ccfaaebe7d8d
Michael Whittaker <mjwhittaker@berkeley.edu>
2018-12-17 15:19:07 -0800

    Finished first implementation of leader election.

--- shared/src/main/scala/zeno/examples/LeaderElection.scala
+++ shared/src/main/scala/zeno/examples/LeaderElection.scala
@@ -1,10 +1,11 @@
 package zeno.examples
 
+import scala.collection.mutable
 import scala.scalajs.js.annotation._
 import zeno.Actor
 import zeno.Logger
-import scala.collection.mutable
 import zeno.ProtoSerializer
+import zeno.TypedActorClient
 
 @JSExportAll
 object LeaderElectionInboundSerializer
@@ -58,8 +59,7 @@ class LeaderElectionActor[Transport <: zeno.Transport[Transport]](
   sealed trait LeaderElectionState
 
   case class LeaderlessFollower(
-      noPingTimer: Transport#Timer,
-      vote: Transport#Address
+      noPingTimer: Transport#Timer
   ) extends LeaderElectionState
 
   case class Follower(
@@ -68,11 +68,11 @@ class LeaderElectionActor[Transport <: zeno.Transport[Transport]](
   ) extends LeaderElectionState
 
   case class Candidate(
-      noVoteTimer: Transport#Timer,
+      notEnoughVotesTimer: Transport#Timer,
       votes: Set[Transport#Address]
   ) extends LeaderElectionState
 
-  case class Leader() extends LeaderElectionState
+  case class Leader(pingTimer: Transport#Timer) extends LeaderElectionState
 
   // Members ///////////////////////////////////////////////////////////////////
   override type InboundMessage = LeaderElectionInbound
@@ -86,12 +86,24 @@ class LeaderElectionActor[Transport <: zeno.Transport[Transport]](
     options.notEnoughVotesTimeoutMax
   )
 
+  val nodes: Map[
+    Transport#Address,
+    TypedActorClient[Transport, LeaderElectionActor[Transport]]
+  ] = {
+    for (address <- addresses)
+      yield
+        (address -> typedActorClient[LeaderElectionActor[Transport]](
+          address,
+          LeaderElectionActor.serializer
+        ))
+  }.toMap
+
   var callbacks: mutable.Buffer[(Transport#Address) => Unit] = mutable.Buffer()
   var round: Int = 0
   var state: LeaderElectionState = {
-    val t = timer("noPingTimer", noPingTimeout(), noPingTimer)
+    val t = noPingTimer()
     t.start()
-    LeaderlessFollower(t, address)
+    LeaderlessFollower(t)
   }
 
   // Callback registration /////////////////////////////////////////////////////
@@ -116,24 +128,231 @@ class LeaderElectionActor[Transport <: zeno.Transport[Transport]](
   }
 
   private def handlePing(src: Transport#Address, ping: Ping): Unit = {
-    ???
+    // If we hear a ping from an earlier round, we ignore it.
+    if (ping.round < round) {
+      return
+    }
+
+    // If we hear from a leader in a larger round, then we immediately become a
+    // follower of that leader.
+    if (ping.round > round) {
+      transitionToFollower(ping.round, src)
+      return
+    }
+
+    state match {
+      case LeaderlessFollower(noPingTimer) => {
+        transitionToFollower(ping.round, src)
+      }
+      case Follower(noPingTimer, leader) => {
+        noPingTimer.reset()
+      }
+      case Candidate(notEnoughVotesTimer, votes) => {
+        transitionToFollower(ping.round, src)
+      }
+      case Leader(pingTimer) => {
+        // We are the leader and received a ping from ourselves. We can just
+        // ignore this ping.
+        logger.check_eq(address, src)
+      }
+    }
   }
 
   private def handleVoteRequest(
       src: Transport#Address,
       voteRequest: VoteRequest
   ): Unit = {
-    ???
+    // If we hear a vote request from an earlier round, we ignore it.
+    if (voteRequest.round < round) {
+      return
+    }
+
+    // If we hear a vote request from a node in a later round, we immediately
+    // become a leaderless follower and vote for that node.
+    if (voteRequest.round > round) {
+      stopTimer(state)
+      round = voteRequest.round
+      val t = noPingTimer()
+      t.start()
+      state = LeaderlessFollower(t)
+      nodes(src).send(LeaderElectionInbound().withVote(Vote(round = round)))
+      return
+    }
+
+    // Otherwise, the vote request is for our current round. In all cases, we
+    // ignore the request.
   }
 
   private def handleVote(src: Transport#Address, vote: Vote): Unit = {
-    ???
+    // If we hear a vote from an earlier round, we ignore it.
+    if (vote.round < round) {
+      return
+    }
+
+    // Hearing a vote from a future round is impossible! We can't hear a vote
+    // in round `r` unless we send a vote request in round `r`. If we're not
+    // yet in round `vote.round`, then we never sent a vote request in round
+    // `vote.round`.
+    if (vote.round > round) {
+      logger.fatal(
+        s"A node received a vote for round ${vote.round} but is only in " +
+          s"round $round."
+      )
+      return
+    }
+
+    state match {
+      case LeaderlessFollower(noPingTimer) => {
+        // If we're a leaderless follower in this round, then we haven't yet
+        // become a candidate in this round. If we haven't yet become a
+        // candidate, we never sent a vote request, so we cannot receive a
+        // vote.
+        logger.fatal(
+          s"A node received a vote in round ${vote.round} but is a " +
+            "leaderless follower."
+        )
+        return
+      }
+      case Follower(noPingTimer, leader) => {
+        // It is possible that we were a candidate in this round, then heard
+        // from a leader in this round and stepped down to follower. In this
+        // case, we simply ignore the vote.
+      }
+      case Candidate(notEnoughVotesTimer, votes) => {
+        val newVotes = votes + src
+
+        // If we've received votes from a majority of the nodes, then we are
+        // the leader for this round. `addresses.size / 2 + 1` is just a
+        // formula for a majority.
+        if (newVotes.size >= (addresses.size / 2 + 1)) {
+          stopTimer(state)
+          val t = pingTimer()
+          t.start()
+          state = Leader(t)
+
+          for (address <- addresses) {
+            nodes(address).send(
+              LeaderElectionInbound().withPing(Ping(round = round))
+            )
+          }
+        }
+      }
+      case Leader(pingTimer) => {
+        // It is possible that a candidate is elected leader and then later
+        // receives some votes. We just ignore these votes.
+      }
+    }
   }
 
-  // Timeouts //////////////////////////////////////////////////////////////////
-  private def pingTimer(): Unit = { ??? }
-  private def noPingTimer(): Unit = { ??? }
-  private def notEnoughVotesTimer(): Unit = { ??? }
+  private def stopTimer(state: LeaderElectionState): Unit = {
+    state match {
+      case LeaderlessFollower(noPingTimer)   => { noPingTimer.stop() }
+      case Follower(noPingTimer, _)          => { noPingTimer.stop() }
+      case Candidate(notEnoughVotesTimer, _) => { notEnoughVotesTimer.stop() }
+      case Leader(pingTimer)                 => { pingTimer.stop() }
+    }
+  }
+
+  private def transitionToFollower(
+      newRound: Int,
+      leader: Transport#Address
+  ): Unit = {
+    stopTimer(state)
+    round = newRound
+    val t = noPingTimer()
+    t.start()
+    state = Follower(t, leader)
+
+    for (callback <- callbacks) {
+      callback(leader)
+    }
+  }
+
+  // Timers ////////////////////////////////////////////////////////////////////
+  private def pingTimer(): Transport#Timer = {
+    // We make `t` a lazy val to avoid the circular definition.
+    lazy val t: Transport#Timer = timer(
+      "pingTimer",
+      options.pingPeriod,
+      () => {
+        for (address <- addresses) {
+          nodes(address).send(
+            LeaderElectionInbound().withPing(Ping(round = round))
+          )
+        }
+        t.start()
+      }
+    )
+    t
+  }
+
+  private def noPingTimer(): Transport#Timer = {
+    timer(
+      "noPingTimer",
+      randomDuration(options.noPingTimeoutMin, options.noPingTimeoutMax),
+      () => {
+        state match {
+          case LeaderlessFollower(noPingTimer) => {
+            transitionToCandidate()
+          }
+          case Follower(noPingTimer, leader) => {
+            transitionToCandidate()
+          }
+          case Candidate(notEnoughVotesTimer, votes) => {
+            logger.fatal("A no ping timer was triggered for a candidate!")
+          }
+          case Leader(pingTimer) => {
+            logger.fatal("A no ping timer was triggered for a leader!")
+          }
+        }
+      }
+    )
+  }
+
+  private def notEnoughVotesTimer(): Transport#Timer = {
+    timer(
+      "notEnoughVotes",
+      randomDuration(
+        options.notEnoughVotesTimeoutMin,
+        options.notEnoughVotesTimeoutMax
+      ),
+      () => {
+        state match {
+          case LeaderlessFollower(noPingTimer) => {
+            logger.fatal(
+              "A not enough votes timer was triggered for a leaderless " +
+                "follower!"
+            )
+          }
+          case Follower(noPingTimer, leader) => {
+            logger.fatal(
+              "A not enough votes timer was triggered for a follower!"
+            )
+          }
+          case Candidate(notEnoughVotesTimer, votes) => {
+            transitionToCandidate()
+          }
+          case Leader(pingTimer) => {
+            logger.fatal("A not enough votes timer was triggered for a leader!")
+          }
+        }
+      }
+    )
+  }
+
+  private def transitionToCandidate(): Unit = {
+    stopTimer(state)
+    round += 1
+    val t = notEnoughVotesTimer()
+    t.start()
+    state = Candidate(t, Set())
+
+    for (address <- addresses) {
+      nodes(address).send(
+        LeaderElectionInbound().withVoteRequest(VoteRequest(round = round))
+      )
+    }
+  }
 
   // Helpers ///////////////////////////////////////////////////////////////////
   // Returns a duration sampled uniformly at random between min (inclusive) and
@@ -159,29 +378,4 @@ class LeaderElectionActor[Transport <: zeno.Transport[Transport]](
       )
     )
   }
-
-  private def pingPeriod(): java.time.Duration = {
-    options.pingPeriod
-  }
-
-  private def noPingTimeout(): java.time.Duration = {
-    randomDuration(options.noPingTimeoutMin, options.noPingTimeoutMax)
-  }
-
-  private def notEnoughVotesTimeout(): java.time.Duration = {
-    randomDuration(
-      options.notEnoughVotesTimeoutMin,
-      options.notEnoughVotesTimeoutMax
-    )
-  }
-  // round = 0
-  // state =
-  // follower
-  //   follower timeout
-  // candidate of responses
-  //   candidate timeout
-  // leader
-  //   leader timeout
-  //
-  // register callback on listen
 }
