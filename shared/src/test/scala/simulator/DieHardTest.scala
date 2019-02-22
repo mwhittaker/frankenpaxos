02e0b6270a00f197260a0d81f1ebcf45e87d96e6
Michael Whittaker <mjwhittaker@berkeley.edu>
2019-02-21 16:44:32 -0800

    Reduced number of runs in the DieHard test.

--- shared/src/test/scala/simulator/DieHardTest.scala
+++ shared/src/test/scala/simulator/DieHardTest.scala
@@ -6,7 +6,7 @@ class DieHardSpec extends FlatSpec {
   "Die Hard jugs" should "always satisfy their type invariants" in {
     val sim = new SimulatedDieHard()
     Simulator
-      .simulate(sim, runLength = 10, numRuns = 1000)
+      .simulate(sim, runLength = 10, numRuns = 100)
       .flatMap(b => Simulator.minimize(sim, b.history)) match {
       case Some(BadHistory(history, error)) =>
         fail(s"Error: $error\n$history")
