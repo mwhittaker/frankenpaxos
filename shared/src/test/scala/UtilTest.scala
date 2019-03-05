0eebe276cd7bb26b3318e881c7c83dc2f3b2f64d
Michael Whittaker <mjwhittaker@berkeley.edu>
2019-03-04 21:56:48 -0800

    Added histogram test.

--- shared/src/test/scala/UtilTest.scala
+++ shared/src/test/scala/UtilTest.scala
@@ -1,8 +1,25 @@
 package frankenpaxos
 
+import collection.mutable
 import org.scalatest._
 
 class UtilSpec extends FlatSpec {
+  "histogram" should "work correctly" in {
+    assertResult(mutable.Map())(Util.histogram(Seq()))
+    assertResult(mutable.Map(1 -> 1, 2 -> 1, 3 -> 1, 4 -> 1)) {
+      Util.histogram(Seq(1, 2, 3, 4))
+    }
+    assertResult(mutable.Map(1 -> 1, 2 -> 1, 3 -> 1, 4 -> 2)) {
+      Util.histogram(Seq(1, 2, 4, 3, 4))
+    }
+    assertResult(mutable.Map(1 -> 2, 2 -> 1, 3 -> 1, 4 -> 2)) {
+      Util.histogram(Seq(4, 1, 2, 1, 3, 4))
+    }
+    assertResult(mutable.Map(1 -> 2, 2 -> 1, 3 -> 1, 4 -> 3)) {
+      Util.histogram(Seq(4, 1, 2, 1, 4, 3, 4))
+    }
+  }
+
   "popularItems" should "work correctly" in {
     assertResult(Set())(Util.popularItems(Seq(), 42))
     assertResult(Set())(Util.popularItems(Seq(1, 2, 3, 4), 2))
