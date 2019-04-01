3b76d3290fd011d1e58064393c6c3243e762da90
Michael Whittaker <mjwhittaker@berkeley.edu>
2019-03-31 20:43:24 -0700

    Added missing Paxos files + proto fix.

---
+++ jvm/src/main/scala/frankenpaxos/paxos/ConfigUtil.scala
@@ -0,0 +1,30 @@
+package frankenpaxos.paxos
+
+import java.net.InetSocketAddress
+import frankenpaxos.NettyTcpAddress
+import frankenpaxos.NettyTcpTransport
+
+object ConfigUtil {
+  def fromProto(proto: NettyConfigProto): Config[NettyTcpTransport] = {
+    Config(
+      f = proto.f,
+      leaderAddresses = proto.leaderAddress.map(
+        hp => NettyTcpAddress(new InetSocketAddress(hp.host, hp.port))
+      ),
+      acceptorAddresses = proto.acceptorAddress.map(
+        hp => NettyTcpAddress(new InetSocketAddress(hp.host, hp.port))
+      )
+    )
+  }
+
+  def fromFile(
+      filename: String
+  ): Config[NettyTcpTransport] = {
+    val source = scala.io.Source.fromFile(filename)
+    try {
+      fromProto(NettyConfigProto.fromAscii(source.mkString))
+    } finally {
+      source.close()
+    }
+  }
+}
---
+++ jvm/src/main/scala/frankenpaxos/paxos/LeaderMain.scala
@@ -0,0 +1,44 @@
+package frankenpaxos.paxos
+
+import java.io.File
+import java.net.InetAddress
+import java.net.InetSocketAddress
+import frankenpaxos.Actor
+import frankenpaxos.NettyTcpAddress
+import frankenpaxos.NettyTcpTransport
+import frankenpaxos.PrintLogger
+
+object LeaderMain extends App {
+  case class Flags(
+      index: Int = -1,
+      paxosConfigFile: File = new File(".")
+  )
+
+  val parser = new scopt.OptionParser[Flags]("") {
+    opt[Int]('i', "index")
+      .required()
+      .valueName("<index>")
+      .action((x, f) => f.copy(index = x))
+      .text("Leader index.")
+
+    opt[File]('c', "config")
+      .required()
+      .valueName("<file>")
+      .action((x, a) => a.copy(paxosConfigFile = x))
+      .text("Configuration file.")
+  }
+
+  val flags: Flags = parser.parse(args, Flags()) match {
+    case Some(flags) =>
+      flags
+    case None =>
+      System.exit(-1)
+      ???
+  }
+
+  val logger = new PrintLogger()
+  val transport = new NettyTcpTransport(logger);
+  val config = ConfigUtil.fromFile(flags.paxosConfigFile.getAbsolutePath())
+  val address = config.leaderAddresses(flags.index)
+  new Leader[NettyTcpTransport](address, transport, logger, config)
+}
