7efc8e1e768d7d6fc0915d774b06c653b219b001
Michael Whittaker <mjwhittaker@berkeley.edu>
2019-04-18 08:48:56 -0700

    Added new flags to FMP Leader executable.
    This commit introduces new command line flags for the Leader's options,
    including thriftiness options, leader election options, and heartbeat
    options.

--- jvm/src/main/scala/frankenpaxos/fastmultipaxos/LeaderMain.scala
+++ jvm/src/main/scala/frankenpaxos/fastmultipaxos/LeaderMain.scala
@@ -9,17 +9,25 @@ import frankenpaxos.statemachine.Flags.stateMachineTypeRead
 import io.prometheus.client.exporter.HTTPServer
 import io.prometheus.client.hotspot.DefaultExports
 import java.io.File
+import scala.concurrent.duration
 
 object LeaderMain extends App {
   case class Flags(
+      // Basic flags.
       index: Int = -1,
-      paxosConfigFile: File = new File("."),
+      configFilename: File = new File("."),
       stateMachineType: statemachine.StateMachineType = statemachine.TRegister,
+      // Monitoring.
       prometheusHost: String = "0.0.0.0",
-      prometheusPort: Int = 8009
+      prometheusPort: Int = 8009,
+      // Options.
+      leaderOptions: LeaderOptions = LeaderOptions.default
   )
 
   val parser = new scopt.OptionParser[Flags]("") {
+    help("help")
+
+    // Basic flags.
     opt[Int]('i', "index")
       .required()
       .valueName("<index>")
@@ -29,7 +37,7 @@ object LeaderMain extends App {
     opt[File]('c', "config")
       .required()
       .valueName("<file>")
-      .action((x, f) => f.copy(paxosConfigFile = x))
+      .action((x, f) => f.copy(configFilename = x))
       .text("Configuration file.")
 
     opt[statemachine.StateMachineType]('s', "state_machine")
@@ -37,6 +45,7 @@ object LeaderMain extends App {
       .action((x, f) => f.copy(stateMachineType = x))
       .text(s"State machine type (default: ${Flags().stateMachineType})")
 
+    // Monitoring.
     opt[String]("prometheus_host")
       .valueName("<host>")
       .action((x, f) => f.copy(prometheusHost = x))
@@ -48,23 +57,144 @@ object LeaderMain extends App {
       .text(
         s"Prometheus port; -1 to disable (default: ${Flags().prometheusPort})"
       )
+
+    // Options.
+    opt[duration.Duration]("options.resendPhase1asTimerPeriod")
+      .action((x, f) => {
+        f.copy(
+          leaderOptions = f.leaderOptions.copy(
+            resendPhase1asTimerPeriod = java.time.Duration.ofNanos(x.toNanos)
+          )
+        )
+      })
+
+    opt[duration.Duration]("options.resendPhase2asTimerPeriod")
+      .action((x, f) => {
+        f.copy(
+          leaderOptions = f.leaderOptions.copy(
+            resendPhase2asTimerPeriod = java.time.Duration.ofNanos(x.toNanos)
+          )
+        )
+      })
+
+    opt[duration.Duration]("options.election.pingPeriod")
+      .action((x, f) => {
+        f.copy(
+          leaderOptions = f.leaderOptions.copy(
+            leaderElectionOptions = f.leaderOptions.leaderElectionOptions.copy(
+              pingPeriod = java.time.Duration.ofNanos(x.toNanos)
+            )
+          )
+        )
+      })
+
+    opt[duration.Duration]("options.election.noPingTimeoutMin")
+      .action((x, f) => {
+        f.copy(
+          leaderOptions = f.leaderOptions.copy(
+            leaderElectionOptions = f.leaderOptions.leaderElectionOptions.copy(
+              noPingTimeoutMin = java.time.Duration.ofNanos(x.toNanos)
+            )
+          )
+        )
+      })
+
+    opt[duration.Duration]("options.election.noPingTimeoutMax")
+      .action((x, f) => {
+        f.copy(
+          leaderOptions = f.leaderOptions.copy(
+            leaderElectionOptions = f.leaderOptions.leaderElectionOptions.copy(
+              noPingTimeoutMax = java.time.Duration.ofNanos(x.toNanos)
+            )
+          )
+        )
+      })
+
+    opt[duration.Duration]("options.election.notEnoughVotesTimeoutMin")
+      .action((x, f) => {
+        f.copy(
+          leaderOptions = f.leaderOptions.copy(
+            leaderElectionOptions = f.leaderOptions.leaderElectionOptions.copy(
+              notEnoughVotesTimeoutMin = java.time.Duration.ofNanos(x.toNanos)
+            )
+          )
+        )
+      })
+
+    opt[duration.Duration]("options.election.notEnoughVotesTimeoutMax")
+      .action((x, f) => {
+        f.copy(
+          leaderOptions = f.leaderOptions.copy(
+            leaderElectionOptions = f.leaderOptions.leaderElectionOptions.copy(
+              notEnoughVotesTimeoutMax = java.time.Duration.ofNanos(x.toNanos)
+            )
+          )
+        )
+      })
+
+    opt[duration.Duration]("options.heartbeat.failPeriod")
+      .action((x, f) => {
+        f.copy(
+          leaderOptions = f.leaderOptions.copy(
+            heartbeatOptions = f.leaderOptions.heartbeatOptions.copy(
+              failPeriod = java.time.Duration.ofNanos(x.toNanos)
+            )
+          )
+        )
+      })
+
+    opt[duration.Duration]("options.heartbeat.successPeriod")
+      .action((x, f) => {
+        f.copy(
+          leaderOptions = f.leaderOptions.copy(
+            heartbeatOptions = f.leaderOptions.heartbeatOptions.copy(
+              successPeriod = java.time.Duration.ofNanos(x.toNanos)
+            )
+          )
+        )
+      })
+
+    opt[Int]("options.heartbeat.numRetries")
+      .action((x, f) => {
+        f.copy(
+          leaderOptions = f.leaderOptions.copy(
+            heartbeatOptions = f.leaderOptions.heartbeatOptions.copy(
+              numRetries = x
+            )
+          )
+        )
+      })
+
+    opt[Double]("options.heartbeat.networkDelayAlpha")
+      .action((x, f) => {
+        f.copy(
+          leaderOptions = f.leaderOptions.copy(
+            heartbeatOptions = f.leaderOptions.heartbeatOptions.copy(
+              networkDelayAlpha = x
+            )
+          )
+        )
+      })
   }
 
   val flags: Flags = parser.parse(args, Flags()) match {
-    case Some(flags) => flags
-    case None        => ???
+    case Some(flags) =>
+      flags
+    case None =>
+      throw new IllegalArgumentException("Could not parse flags.")
   }
 
   val logger = new PrintLogger()
-  val transport = new NettyTcpTransport(logger);
-  val config = ConfigUtil.fromFile(flags.paxosConfigFile.getAbsolutePath())
+  val transport = new NettyTcpTransport(logger)
+  val config = ConfigUtil.fromFile(flags.configFilename.getAbsolutePath())
   val address = config.leaderAddresses(flags.index)
   val stateMachine = statemachine.Flags.make(flags.stateMachineType)
   val server = new Leader[NettyTcpTransport](address,
                                              transport,
                                              logger,
                                              config,
-                                             stateMachine)
+                                             stateMachine,
+                                             flags.leaderOptions)
 
   if (flags.prometheusPort != -1) {
     DefaultExports.initialize()
