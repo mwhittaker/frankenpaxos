0e0aee049133447317d4ddf10b129b938931e163
Michael Whittaker <mjwhittaker@berkeley.edu>
2019-06-12 14:12:45 -0700

    Documented Timer.

--- shared/src/main/scala/frankenpaxos/Timer.scala
+++ shared/src/main/scala/frankenpaxos/Timer.scala
@@ -1,16 +1,42 @@
 package frankenpaxos
 
-import scala.scalajs.js.annotation._;
+import scala.scalajs.js.annotation.JSExportAll
 
-// TODO(mwhittaker): Clarify semantics of what happens when you start a timer
-// that's already started or stop a timer that's already stopped.
+// A Timer is a function that gets invoked after a specified timeout.
+// Typically, you construct a Timer using the `timer` method within actor. For
+// example, the following actor prints "hello" every second.
+//
+//   class HelloActor[Transport <: frankenpaxos.Transport[Transport]](
+//       val address: Transport#Address,
+//       val transport: Transport,
+//       val logger: Logger
+//   ) extends Actor(address, transport, logger) {
+//     ...
+//
+//     val helloTimer = timer(
+//       "hello",
+//       java.time.Duration.ofSeconds(1),
+//       () => { println("hello") }
+//     )
+//   }
 @JSExportAll
 trait Timer {
+  // Returns the name of the timer. Note that names do NOT uniquely identify
+  // timers. Two timers can have the same name. Timer have names to make
+  // debugging easier.
   def name(): String
+
+  // Start the timer. If the timer is already started, calling start doesn't do
+  // anything.
   def start(): Unit
+
+  // Stop the timer. If the timer is already stopped, calling stop doesn't do
+  // anything.
   def stop(): Unit
+
+  // Stop and then start the timer.
   def reset(): Unit = {
-    stop();
-    start();
+    stop()
+    start()
   }
 }
