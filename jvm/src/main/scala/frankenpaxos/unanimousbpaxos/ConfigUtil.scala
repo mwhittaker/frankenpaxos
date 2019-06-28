0f97646a1847d3401b83bdad478d2b0c6296fe86
Michael Whittaker <mjwhittaker@berkeley.edu>
2019-06-28 11:21:41 -0700

    Added Unanimous BPaxos executables.

---
+++ jvm/src/main/scala/frankenpaxos/unanimousbpaxos/ConfigUtil.scala
@@ -0,0 +1,28 @@
+package frankenpaxos.unanimousbpaxos
+
+import frankenpaxos.NettyTcpAddress
+import frankenpaxos.NettyTcpTransport
+import java.net.InetSocketAddress
+
+object ConfigUtil {
+  def fromProto(proto: NettyConfigProto): Config[NettyTcpTransport] = {
+    def addr(hostPort: HostPortProto): NettyTcpAddress =
+      NettyTcpAddress(new InetSocketAddress(hostPort.host, hostPort.port))
+
+    Config(
+      f = proto.f,
+      leaderAddresses = proto.leaderAddress.map(addr),
+      depServiceNodeAddresses = proto.depServiceNodeAddress.map(addr),
+      acceptorAddresses = proto.acceptorAddress.map(addr)
+    )
+  }
+
+  def fromFile(filename: String): Config[NettyTcpTransport] = {
+    val source = scala.io.Source.fromFile(filename)
+    try {
+      fromProto(NettyConfigProto.fromAscii(source.mkString))
+    } finally {
+      source.close()
+    }
+  }
+}
