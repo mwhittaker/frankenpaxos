30a075b85d6dfc42d05ab618fb986f3842ff8505
Michael Whittaker <mjwhittaker@berkeley.edu>
2019-10-29 18:01:45 -0700

    Added echo to benchmark_smoke.

--- scripts/benchmark_smoke.sh
+++ scripts/benchmark_smoke.sh
@@ -3,14 +3,15 @@
 set -euo pipefail
 
 main() {
-    for protocol in unreplicated batchedunreplicated multipaxos mencius \
-                    fastmultipaxos epaxos simplebpaxos superbpaxos \
-                    simplegcbpaxos unanimousbpaxos; do
+    for protocol in unreplicated batchedunreplicated multipaxos \
+                    supermultipaxos mencius fastmultipaxos epaxos simplebpaxos \
+                    superbpaxos simplegcbpaxos unanimousbpaxos; do
         echo "Running $protocol."
         python -m "benchmarks.${protocol}.smoke" \
             -m \
             --cluster "benchmarks/${protocol}/local_cluster.json" \
             -i ~/.ssh/id_rsa
+        echo
     done
 }
 
