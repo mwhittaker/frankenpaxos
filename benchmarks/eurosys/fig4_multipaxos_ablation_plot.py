ad2db1734839a3c8ac1db79d27f09f42d3b9f2a4
Michael Whittaker <mjwhittaker@berkeley.edu>
2019-11-02 02:45:13 +0000

    Added final ablation graph.

--- benchmarks/eurosys/fig4_multipaxos_ablation_plot.py
+++ benchmarks/eurosys/fig4_multipaxos_ablation_plot.py
@@ -34,86 +34,22 @@ def add_num_clients(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-# def latency_figure(output_filename: str, superbpaxos_df: pd.DataFrame,
-#                    simplebpaxos_df: pd.DataFrame) -> None:
-#     fig, ax = plt.subplots(1, 1, figsize=(6.4, 4.8))
-#     labels = ('coupled', '3 leaders', '4 leaders', '5 leaders', '6 leaders',
-#               '7 leaders')
-#     latencies = [
-#         avg_latency(superbpaxos_df),
-#         avg_latency(simplebpaxos_df[simplebpaxos_df['num_leaders'] == 3]),
-#         avg_latency(simplebpaxos_df[simplebpaxos_df['num_leaders'] == 4]),
-#         avg_latency(simplebpaxos_df[simplebpaxos_df['num_leaders'] == 5]),
-#         avg_latency(simplebpaxos_df[simplebpaxos_df['num_leaders'] == 6]),
-#         avg_latency(simplebpaxos_df[simplebpaxos_df['num_leaders'] == 7]),
-#     ]
-#     yerr = [
-#         std_latency(superbpaxos_df),
-#         std_latency(simplebpaxos_df[simplebpaxos_df['num_leaders'] == 3]),
-#         std_latency(simplebpaxos_df[simplebpaxos_df['num_leaders'] == 4]),
-#         std_latency(simplebpaxos_df[simplebpaxos_df['num_leaders'] == 5]),
-#         std_latency(simplebpaxos_df[simplebpaxos_df['num_leaders'] == 6]),
-#         std_latency(simplebpaxos_df[simplebpaxos_df['num_leaders'] == 7]),
-#     ]
-#     x_pos = range(len(latencies))
-#     ax.bar(x_pos, latencies, yerr=yerr, align='center', capsize=10)
-#     ax.set_xticks(x_pos)
-#     ax.set_xticklabels(labels, rotation=-45)
-#     ax.set_title('')
-#     ax.set_xlabel('')
-#     ax.set_ylabel('Median latency (ms)')
-#     ax.legend(loc='best')
-#     fig.savefig(output_filename, bbox_inches='tight')
-#     print(f'Wrote plot to {output_filename}.')
-
-
 def barchart(output_filename: str,
              labels: List[str],
              data: List[float],
-             yerr: List[float]) -> None:
+             yerr: List[float],
+             color: List[str]) -> None:
+
     fig, ax = plt.subplots(1, 1, figsize=(6.4, 4.8))
     x_pos = range(len(data))
-    ax.bar(x_pos, data, yerr=yerr, align='center', capsize=10)
+    ax.bar(x_pos, data, yerr=yerr, align='center', capsize=10, color=color)
     ax.set_xticks(x_pos)
     ax.set_xticklabels(labels, rotation=-45, ha='left')
     ax.set_title('')
     ax.set_xlabel('')
     ax.set_ylabel('Throughput (commands per second)')
-    # ax.legend(loc='best')
     fig.savefig(output_filename, bbox_inches='tight')
     print(f'Wrote plot to {output_filename}.')
-
-# def throughput_figure(output_filename: str, superbpaxos_df: pd.DataFrame,
-#                       simplebpaxos_df: pd.DataFrame) -> None:
-#     fig, ax = plt.subplots(1, 1, figsize=(6.4, 4.8))
-#     labels = ('coupled', '3 leaders', '4 leaders', '5 leaders', '6 leaders',
-#               '7 leaders')
-#     latencies = [
-#         avg_tput(superbpaxos_df),
-#         avg_tput(simplebpaxos_df[simplebpaxos_df['num_leaders'] == 3]),
-#         avg_tput(simplebpaxos_df[simplebpaxos_df['num_leaders'] == 4]),
-#         avg_tput(simplebpaxos_df[simplebpaxos_df['num_leaders'] == 5]),
-#         avg_tput(simplebpaxos_df[simplebpaxos_df['num_leaders'] == 6]),
-#         avg_tput(simplebpaxos_df[simplebpaxos_df['num_leaders'] == 7]),
-#     ]
-#     yerr = [
-#         std_tput(superbpaxos_df),
-#         std_tput(simplebpaxos_df[simplebpaxos_df['num_leaders'] == 3]),
-#         std_tput(simplebpaxos_df[simplebpaxos_df['num_leaders'] == 4]),
-#         std_tput(simplebpaxos_df[simplebpaxos_df['num_leaders'] == 5]),
-#         std_tput(simplebpaxos_df[simplebpaxos_df['num_leaders'] == 6]),
-#         std_tput(simplebpaxos_df[simplebpaxos_df['num_leaders'] == 7]),
-#     ]
-#     x_pos = range(len(latencies))
-#     ax.bar(x_pos, latencies, yerr=yerr, align='center', capsize=10)
-#     ax.set_xticks(x_pos)
-#     ax.set_xticklabels(labels, rotation=-45)
-#     ax.set_title('')
-#     ax.set_xlabel('')
-#     ax.set_ylabel('Throughput (commands per second)')
-#     ax.legend(loc='best')
-#     fig.savefig(output_filename, bbox_inches='tight')
-#     print(f'Wrote plot to {output_filename}.')
 
 
 def main(args) -> None:
@@ -121,20 +57,20 @@ def main(args) -> None:
         add_num_clients(pd.read_csv(args.unbatched_coupled_multipaxos_results))
     unbatched_df = \
         add_num_clients(pd.read_csv(args.unbatched_multipaxos_results))
-    # batched_super_df = \
-    #     add_num_clients(pd.read_csv(args.batched_coupled_multipaxos_results))
-    # batched_df = \
-    #     add_num_clients(pd.read_csv(args.batched_multipaxos_results))
+    batched_super_df = \
+        add_num_clients(pd.read_csv(args.batched_coupled_multipaxos_results))
+    batched_df = \
+        add_num_clients(pd.read_csv(args.batched_multipaxos_results))
 
-    # We only look at data with 10 * 100 clients.
+    # We only look at data with 10 * 100 clients batched and 20 * 200 batched.
     unbatched_super_df = \
         unbatched_super_df[unbatched_super_df['num_clients'] == 1000]
     unbatched_df = \
         unbatched_df[unbatched_df['num_clients'] == 1000]
-    # batched_super_df = \
-    #     batched_super_df[batched_super_df['num_clients'] == 1000]
-    # batched_df = \
-    #     batched_df[batched_df['num_clients'] == 1000]
+    batched_super_df = \
+        batched_super_df[batched_super_df['num_clients'] == 4000]
+    batched_df = \
+        batched_df[batched_df['num_clients'] == 4000]
 
     barchart(
         output_filename = args.output_unbatched_throughput,
@@ -174,24 +110,55 @@ def main(args) -> None:
                                   (unbatched_df['num_acceptor_groups'] == 1)]),
             std_tput(unbatched_df[(unbatched_df['num_proxy_leaders'] == 8) &
                                   (unbatched_df['num_acceptor_groups'] == 2)]),
-        ]
+        ],
+        color = [
+            'C0',
+            'C1',
+            'C2',
+            'C2',
+            'C2',
+            'C2',
+            'C2',
+            'C2',
+            'C3',
+        ],
     )
 
-    # latency_figure(
-    #     args.output_low_load_latency,
-    #     superbpaxos_df[superbpaxos_df['num_clients'] == 1],
-    #     simplebpaxos_df[simplebpaxos_df['num_clients'] == 1],
-    # )
-    # latency_figure(
-    #     args.output_high_load_latency,
-    #     superbpaxos_df[superbpaxos_df['num_clients'] == 600],
-    #     simplebpaxos_df[simplebpaxos_df['num_clients'] == 600],
-    # )
-    # throughput_figure(
-    #     args.output_high_load_throughput,
-    #     superbpaxos_df[superbpaxos_df['num_clients'] == 600],
-    #     simplebpaxos_df[simplebpaxos_df['num_clients'] == 600],
-    # )
+    barchart(
+        output_filename = args.output_batched_throughput,
+        labels = [
+            'coupled',
+            'decoupled',
+            '3 proxy replicas',
+            '4 proxy replicas',
+            '5 proxy replicas',
+            '6 proxy replicas',
+        ],
+        data = [
+            avg_tput(batched_super_df),
+            avg_tput(batched_df[batched_df['num_proxy_replicas'] == 2]),
+            avg_tput(batched_df[batched_df['num_proxy_replicas'] == 3]),
+            avg_tput(batched_df[batched_df['num_proxy_replicas'] == 4]),
+            avg_tput(batched_df[batched_df['num_proxy_replicas'] == 5]),
+            avg_tput(batched_df[batched_df['num_proxy_replicas'] == 6]),
+        ],
+        yerr = [
+            std_tput(batched_super_df),
+            std_tput(batched_df[batched_df['num_proxy_replicas'] == 2]),
+            std_tput(batched_df[batched_df['num_proxy_replicas'] == 3]),
+            std_tput(batched_df[batched_df['num_proxy_replicas'] == 4]),
+            std_tput(batched_df[batched_df['num_proxy_replicas'] == 5]),
+            std_tput(batched_df[batched_df['num_proxy_replicas'] == 6]),
+        ],
+        color = [
+            'C0',
+            'C1',
+            'C2',
+            'C2',
+            'C2',
+            'C2',
+        ],
+    )
 
 
 def get_parser() -> argparse.ArgumentParser:
