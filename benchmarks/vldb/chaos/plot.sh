932fa98074f3d8ad0986ca0099e296eef04cd8ae
Michael Whittaker <mjwhittaker@berkeley.edu>
2020-07-02 18:30:47 +0000

    Added chaos results.

---
+++ benchmarks/vldb/chaos/plot.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+main() {
+    local -r d="$(dirname $0)"
+    python -m benchmarks.vldb.chaos.plot \
+        --sample_every "100" \
+        --drop_head "10" \
+        --drop_tail "10" \
+        --n1 <(gunzip -c "$d/n1.csv.gz") \
+        --n4 <(gunzip -c "$d/n4.csv.gz") \
+        --n8 <(gunzip -c "$d/n8.csv.gz") \
+        --output "$d/../output/chaos.pdf"
+}
+
+main "$@"
