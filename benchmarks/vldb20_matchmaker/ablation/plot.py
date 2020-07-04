14cba189d6b45c0a0b8d4d8cc1ae33dc5e1583fb
Michael Whittaker <mjwhittaker@berkeley.edu>
2020-07-04 18:22:01 +0000

    Fixed ablation graph.

--- benchmarks/vldb20_matchmaker/ablation/plot.py
+++ benchmarks/vldb20_matchmaker/ablation/plot.py
@@ -16,9 +16,6 @@ import pandas as pd
 import re
 
 
-MARKERS = itertools.cycle(['o', '*', '^', 's'])
-
-
 def read_data(file,
               drop_head: float,
               drop_tail: float) -> Tuple[pd.DataFrame, Any]:
@@ -40,28 +37,25 @@ def read_data(file,
     return (df, new_start_time)
 
 
-def plot_throughput(ax: plt.Axes, label: str, marker: str,
+def plot_throughput(ax: plt.Axes, label: str, color: str,
                     s: pd.Series, sample_every: int) -> None:
-    tput = pd_util.throughput(s, 500, trim=True)[::sample_every]
+    tput = pd_util.throughput(s, 250, trim=True)[::sample_every]
     ax.plot_date(tput.index, tput,
-                 fmt='-', marker=marker, markevery=0.4, label='_nolegend_')
+                 fmt='-', color=color, markevery=0.4, label='_nolegend_')
 
 
-def plot_latency(ax: plt.Axes, label: str, marker: str,
+def plot_latency(ax: plt.Axes, label: str, color: str,
                  s: pd.Series, sample_every: int) -> None:
     median = s.rolling('500ms').median()
-    # p95 = s.rolling('500ms').quantile(0.95)
     max_latency = s.rolling('500ms').max()
 
-    ax.set_yscale('log')
+    # ax.set_yscale('log')
     line = ax.plot_date(s.index[::sample_every], median[::sample_every],
-                        fmt='-', marker=marker, markevery=0.2, label=label)[0]
-    ax.plot_date(s.index[::sample_every], max_latency[::sample_every],
-                 fmt='--', marker=marker, markevery=0.5, color=line.get_color(),
-                 label='_nolegend_')
-    # ax.fill_between(s.index[::sample_every], median[::sample_every],
-    #                 max_latency[::sample_every], color=line.get_color(),
-    #                 alpha=0.5)
+                        fmt='-', color=color, markevery=0.2, label=label)[0]
+    # ax.plot_date(s.index[::sample_every], max_latency[::sample_every],
+    #              fmt='--', color=color, label='_nolegend_')
+    ax.fill_between(s.index[::sample_every], median[::sample_every],
+                    max_latency[::sample_every], color=line.get_color())
 
 
 def plot(baseline: pd.DataFrame,
@@ -72,39 +66,16 @@ def plot(baseline: pd.DataFrame,
          start_time,
          sample_every: int):
     # Create figure.
-    num_plots = 2
-    fig, ax = plt.subplots(num_plots, 1, figsize=(6.4, num_plots * 4.8 * 0.5),
-                           sharex=True)
-
-    # Plot data.
-    plot_latency(ax[0], 'No Opts', next(MARKERS),
-                 baseline['latency_nanos'] / 1e6, sample_every)
-    plot_latency(ax[0], 'Opt 3', next(MARKERS),
-                 gc['latency_nanos'] / 1e6, sample_every)
-    plot_latency(ax[0], 'Opt 2,3', next(MARKERS),
-                 phase1['latency_nanos'] / 1e6, sample_every)
-    plot_latency(ax[0], 'Opt 1,2,3', next(MARKERS),
-                 matchmaking['latency_nanos'] / 1e6, sample_every)
-    plot_throughput(ax[1], 'No Opts', next(MARKERS),
-                    baseline['delta'], sample_every)
-    plot_throughput(ax[1], 'Opt 3', next(MARKERS),
-                    gc['delta'], sample_every)
-    plot_throughput(ax[1], 'Opt 2,3', next(MARKERS),
-                    phase1['delta'], sample_every)
-    plot_throughput(ax[1], 'Opt 1,2,3', next(MARKERS),
-                    matchmaking['delta'], sample_every)
-
-    # Format x ticks nicely.
-    for axes in ax:
-        axes.grid()
-        # axes.legend(loc='best')
-        axes.xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%-M:%S'))
-        for label in axes.get_xticklabels():
-            label.set_ha('left')
-            label.set_rotation(-20)
+    num_rows = 2
+    num_columns = 4
+    fig, ax = plt.subplots(num_rows, num_columns,
+                           figsize=(num_columns * 6.4 * 0.5, num_rows * 4.8 * 0.5),
+                           sharex=True, sharey='row')
 
     # Draw vertical reconfiguration lines. The times are hardcoded and taken
-    # from the Driver's output files. I know that's super jank, but oh well.
+    # from the Driver's output files. I know that's super jank, but oh well. We
+    # draw the verical lines before the plots, so that the vertical lines don't
+    # hide the latency spikes.
     origin = datetime.datetime(1970, 1, 1, second=0)
     naive_start_time = start_time.to_pydatetime().replace(tzinfo=None)
     reconfigurations = [
@@ -115,22 +86,48 @@ def plot(baseline: pd.DataFrame,
         datetime.datetime(2020, 6, 25, hour=16, minute=34, second=8, microsecond=130000),
     ]
 
-    for axes in ax:
-        for t in reconfigurations:
-            axes.axvline(x=origin + (t - naive_start_time), color='black')
+    for row in ax:
+        for axes in row:
+            for t in reconfigurations:
+                axes.axvline(x=origin + (t - naive_start_time), color='black',
+                             alpha=0.5)
 
-    # Write legend.
-    handles, labels = ax[0].get_legend_handles_labels()
-    legend = fig.legend(handles, labels,
-                        bbox_to_anchor=(0.5, 1), loc='center', ncol=2)
+    # Plot data.
+    plot_latency(ax[0][0], 'No Opts', 'C0', baseline['latency_nanos'] / 1e6, sample_every)
+    plot_latency(ax[0][1], 'Opt 3', 'C1', gc['latency_nanos'] / 1e6, sample_every)
+    plot_latency(ax[0][2], 'Opt 2,3', 'C2', phase1['latency_nanos'] / 1e6, sample_every)
+    plot_latency(ax[0][3], 'Opt 1,2,3', 'C3', matchmaking['latency_nanos'] / 1e6, sample_every)
+    plot_throughput(ax[1][0], 'No Opts', 'C0', baseline['delta'], sample_every)
+    plot_throughput(ax[1][1], 'Opt 3', 'C1', gc['delta'], sample_every)
+    plot_throughput(ax[1][2], 'Opt 2,3', 'C2', phase1['delta'], sample_every)
+    plot_throughput(ax[1][3], 'Opt 1,2,3', 'C3', matchmaking['delta'], sample_every)
+
+    # Format x ticks nicely.
+    for row in ax:
+        for axes in row:
+            axes.grid()
+            # axes.legend(loc='best')
+            axes.xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%-M:%S'))
+            for label in axes.get_xticklabels():
+                # label.set_ha('left')
+                label.set_ha('center')
+                label.set_rotation(-90)
+
+    # Write titles.
+    ax[0][0].set_title('No optimizations')
+    ax[0][1].set_title('Optimization 3')
+    ax[0][2].set_title('Optimizations 2, 3')
+    ax[0][3].set_title('Optimizations 1, 2, 3')
 
     # Save figures.
-    ax[1].set_xlabel('Time')
-    ax[0].set_ylabel('Latency (ms)')
-    ax[1].set_ylabel('Throughput\n(cmds/second)')
+    for axes in ax[1]:
+        axes.set_xlabel('Time')
+    ax[0][0].set_ylabel('Latency (ms)')
+    ax[1][0].set_ylabel('Throughput\n(cmds/second)')
     fig.set_tight_layout(True)
-    fig.savefig(output_filename, bbox_extra_artists=(legend,),
-                bbox_inches='tight')
+    # fig.savefig(output_filename, bbox_extra_artists=(legend,),
+    #             bbox_inches='tight')
+    fig.savefig(output_filename, bbox_inches='tight')
     print(f'Wrote plot to {output_filename}.')
 
 
