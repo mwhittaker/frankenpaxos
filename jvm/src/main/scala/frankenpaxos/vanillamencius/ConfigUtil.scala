3e62587202ce6e8885f6380baf7cda2458ebca73
Michael Whittaker <mjwhittaker@berkeley.edu>
2020-07-29 12:22:28 -0700

    Added vanilla mencius jvm files.

---
+++ jvm/src/main/scala/frankenpaxos/vanillamencius/ConfigUtil.scala
@@ -0,0 +1,27 @@
+package frankenpaxos.vanillamencius
+
+import frankenpaxos.NettyTcpAddress
+import frankenpaxos.NettyTcpTransport
+import java.net.InetSocketAddress
+
+object ConfigUtil {
+  def fromProto(proto: NettyConfigProto): Config[NettyTcpTransport] = {
+    def addr(hostPort: HostPortProto): NettyTcpAddress =
+      NettyTcpAddress(new InetSocketAddress(hostPort.host, hostPort.port))
+
+    Config(
+      f = proto.f,
+      serverAddresses = proto.serverAddress.map(addr),
+      heartbeatAddresses = proto.heartbeatAddress.map(addr)
+    )
+  }
+
+  def fromFile(filename: String): Config[NettyTcpTransport] = {
+    val source = scala.io.Source.fromFile(filename)
+    try {
+      fromProto(NettyConfigProto.fromAscii(source.mkString))
+    } finally {
+      source.close()
+    }
+  }
+}
---
+++ jvm/src/main/scala/frankenpaxos/vanillamencius/ServerMain.scala
@@ -0,0 +1,119 @@
+package frankenpaxos.vanillamencius
+
+import frankenpaxos.Actor
+import frankenpaxos.Flags.durationRead
+import frankenpaxos.LogLevel
+import frankenpaxos.NettyTcpAddress
+import frankenpaxos.NettyTcpTransport
+import frankenpaxos.PrintLogger
+import frankenpaxos.PrometheusUtil
+import frankenpaxos.heartbeat.HeartbeatOptions
+import frankenpaxos.statemachine
+import frankenpaxos.statemachine.AppendLog
+import frankenpaxos.statemachine.StateMachine
+import frankenpaxos.statemachine.StateMachine.read
+import io.prometheus.client.exporter.HTTPServer
+import io.prometheus.client.hotspot.DefaultExports
+import java.io.File
+import java.net.InetAddress
+import java.net.InetSocketAddress
+import scala.concurrent.duration
+
+object ServerMain extends App {
+  case class Flags(
+      // Basic flags.
+      index: Int = -1,
+      configFile: File = new File("."),
+      logLevel: frankenpaxos.LogLevel = frankenpaxos.LogDebug,
+      stateMachine: StateMachine = new statemachine.Noop(),
+      // Monitoring.
+      prometheusHost: String = "0.0.0.0",
+      prometheusPort: Int = 8009,
+      // Options.
+      options: ServerOptions = ServerOptions.default
+  )
+
+  implicit class OptionsWrapper[A](o: scopt.OptionDef[A, Flags]) {
+    def optionAction(
+        f: (A, ServerOptions) => ServerOptions
+    ): scopt.OptionDef[A, Flags] =
+      o.action((x, flags) => flags.copy(options = f(x, flags.options)))
+  }
+
+  implicit class HeartbeatOptionsWrapper[A](o: scopt.OptionDef[A, Flags]) {
+    def heartbeatOptionAction(
+        f: (A, HeartbeatOptions) => HeartbeatOptions
+    ): scopt.OptionDef[A, Flags] =
+      o.action(
+        (x, flags) =>
+          flags.copy(
+            options = flags.options
+              .copy(heartbeatOptions = f(x, flags.options.heartbeatOptions))
+          )
+      )
+  }
+
+  val parser = new scopt.OptionParser[Flags]("") {
+    help("help")
+
+    // Basic flags.
+    opt[Int]("index").required().action((x, f) => f.copy(index = x))
+    opt[File]("config").required().action((x, f) => f.copy(configFile = x))
+    opt[LogLevel]("log_level").required().action((x, f) => f.copy(logLevel = x))
+    opt[StateMachine]("state_machine")
+      .required()
+      .action((x, f) => f.copy(stateMachine = x))
+
+    // Monitoring.
+    opt[String]("prometheus_host")
+      .action((x, f) => f.copy(prometheusHost = x))
+    opt[Int]("prometheus_port")
+      .action((x, f) => f.copy(prometheusPort = x))
+      .text(s"-1 to disable")
+
+    // Options.
+    opt[Int]("options.beta")
+      .optionAction((x, o) => o.copy(beta = x))
+    opt[java.time.Duration]("options.resendPhase1asPeriod")
+      .optionAction((x, o) => o.copy(resendPhase1asPeriod = x))
+    opt[java.time.Duration]("options.flushSkipSlotsPeriod")
+      .optionAction((x, o) => o.copy(flushSkipSlotsPeriod = x))
+    opt[java.time.Duration]("options.revokeMinPeriod")
+      .optionAction((x, o) => o.copy(revokeMinPeriod = x))
+    opt[java.time.Duration]("options.revokeMaxPeriod")
+      .optionAction((x, o) => o.copy(revokeMaxPeriod = x))
+    opt[Int]("options.logGrowSize")
+      .optionAction((x, o) => o.copy(logGrowSize = x))
+    opt[java.time.Duration]("options.heartbeat.failPeriod")
+      .heartbeatOptionAction((x, o) => o.copy(failPeriod = x))
+    opt[java.time.Duration]("options.heartbeat.successPeriod")
+      .heartbeatOptionAction((x, o) => o.copy(successPeriod = x))
+    opt[Int]("options.heartbeat.numRetries")
+      .heartbeatOptionAction((x, o) => o.copy(numRetries = x))
+    opt[Double]("options.heartbeat.networkDelayAlpha")
+      .heartbeatOptionAction((x, o) => o.copy(networkDelayAlpha = x))
+  }
+
+  // Parse flags.
+  val flags: Flags = parser.parse(args, Flags()) match {
+    case Some(flags) =>
+      flags
+    case None =>
+      throw new IllegalArgumentException("Could not parse flags.")
+  }
+
+  // Construct server.
+  val logger = new PrintLogger(flags.logLevel)
+  val config = ConfigUtil.fromFile(flags.configFile.getAbsolutePath())
+  val server = new Server[NettyTcpTransport](
+    address = config.serverAddresses(flags.index),
+    transport = new NettyTcpTransport(logger),
+    logger = logger,
+    stateMachine = flags.stateMachine,
+    config = config,
+    options = flags.options
+  )
+
+  // Start Prometheus.
+  PrometheusUtil.server(flags.prometheusHost, flags.prometheusPort)
+}
