118c73f84f2a6835a9489a1d3ade721ef9a96e55
Michael Whittaker <mjwhittaker@berkeley.edu>
2020-10-21 03:42:57 +0000

    Progress on batched lt.

---
+++ benchmarks/vldb21_compartmentalized/batched_compartmentalized_lt/benchmark.py
@@ -0,0 +1,234 @@
+from ...multipaxos.multipaxos import *
+
+
+def main(args) -> None:
+    class Suite(MultiPaxosSuite):
+        def args(self) -> Dict[Any, Any]:
+            return vars(args)
+
+        def inputs(self) -> Collection[Input]:
+            return [
+                Input(
+                    f = 1,
+                    num_client_procs = num_client_procs,
+                    num_warmup_clients_per_proc = num_clients_per_proc,
+                    num_clients_per_proc = num_clients_per_proc,
+                    num_batchers = num_batchers,
+                    num_read_batchers = 0,
+                    num_leaders = 2,
+                    num_proxy_leaders = num_proxy_leaders,
+                    num_acceptor_groups = num_acceptor_groups,
+                    num_acceptors_per_group = num_acceptors_per_group,
+                    num_replicas = num_replicas,
+                    num_proxy_replicas = num_proxy_replicas,
+                    flexible = flexible,
+                    distribution_scheme = DistributionScheme.HASH,
+                    client_jvm_heap_size = '8g',
+                    batcher_jvm_heap_size = '12g',
+                    read_batcher_jvm_heap_size = '12g',
+                    leader_jvm_heap_size = '12g',
+                    proxy_leader_jvm_heap_size = '12g',
+                    acceptor_jvm_heap_size = '12g',
+                    replica_jvm_heap_size = '12g',
+                    proxy_replica_jvm_heap_size = '12g',
+                    measurement_group_size = 100,
+                    warmup_duration = datetime.timedelta(seconds=10),
+                    warmup_timeout = datetime.timedelta(seconds=15),
+                    warmup_sleep = datetime.timedelta(seconds=5),
+                    duration = datetime.timedelta(seconds=15),
+                    timeout = datetime.timedelta(seconds=20),
+                    client_lag = datetime.timedelta(seconds=5),
+                    state_machine = 'KeyValueStore',
+                    predetermined_read_fraction = -1,
+                    workload_label = 'write_only',
+                    workload =
+                      read_write_workload.UniformReadWriteWorkload(
+                        num_keys=1,
+                        read_fraction=0.0,
+                        write_size_mean=16,
+                        write_size_std=0),
+                    read_workload =
+                      read_write_workload.UniformReadWriteWorkload(
+                        num_keys=1,
+                        read_fraction=1.0,
+                        write_size_mean=16,
+                        write_size_std=0),
+                    write_workload =
+                      read_write_workload.UniformReadWriteWorkload(
+                        num_keys=1,
+                        read_fraction=0.0,
+                        write_size_mean=16,
+                        write_size_std=0),
+                    read_consistency = 'linearizable',
+                    profiled = args.profile,
+                    monitored = args.monitor,
+                    prometheus_scrape_interval =
+                        datetime.timedelta(milliseconds=200),
+                    batcher_options = BatcherOptions(
+                        batch_size = batch_size,
+                    ),
+                    batcher_log_level = args.log_level,
+                    read_batcher_options = ReadBatcherOptions(
+                        read_batching_scheme = "size,1,10s",
+                        unsafe_read_at_first_slot = False,
+                        unsafe_read_at_i = False,
+                    ),
+                    read_batcher_log_level = args.log_level,
+                    leader_options = LeaderOptions(
+                        resend_phase1as_period = datetime.timedelta(seconds=1),
+                        flush_phase2as_every_n = 1,
+                        election_options = ElectionOptions(
+                            ping_period = datetime.timedelta(seconds=60),
+                            no_ping_timeout_min = \
+                                datetime.timedelta(seconds=120),
+                            no_ping_timeout_max = \
+                                datetime.timedelta(seconds=240),
+                        ),
+                    ),
+                    leader_log_level = args.log_level,
+                    proxy_leader_options = ProxyLeaderOptions(
+                        flush_phase2as_every_n = 1,
+                    ),
+                    proxy_leader_log_level = args.log_level,
+                    acceptor_options = AcceptorOptions(),
+                    acceptor_log_level = args.log_level,
+                    replica_options = ReplicaOptions(
+                        log_grow_size = 5000,
+                        unsafe_dont_use_client_table = False,
+                        send_chosen_watermark_every_n_entries = 100,
+                        recover_log_entry_min_period = \
+                            datetime.timedelta(seconds=2),
+                        recover_log_entry_max_period = \
+                            datetime.timedelta(seconds=5),
+                        unsafe_dont_recover = False,
+                    ),
+                    replica_log_level = args.log_level,
+                    proxy_replica_options = ProxyReplicaOptions(
+                        # flush_every_n = 1,
+                        batch_flush = batch_flush,
+                    ),
+                    proxy_replica_log_level = args.log_level,
+                    client_options = ClientOptions(
+                        resend_client_request_period =
+                            datetime.timedelta(seconds=1),
+                        resend_max_slot_requests_period =
+                            datetime.timedelta(seconds=1),
+                        resend_read_request_period =
+                            datetime.timedelta(seconds=1),
+                        resend_sequential_read_request_period =
+                            datetime.timedelta(seconds=1),
+                        resend_eventual_read_request_period =
+                            datetime.timedelta(seconds=1),
+                        unsafe_read_at_first_slot = False,
+                        unsafe_read_at_i = False,
+                        flush_writes_every_n = 1,
+                        flush_reads_every_n = 1,
+                    ),
+                    client_log_level = args.log_level,
+                )
+
+                # Hyperparamter tuning.
+                #
+                # - batch_flush = True increases throughput a lot.
+                # - Increasing the batch size can help throughput. A batch size
+                #   of 10 is worse than a batch size of 50.
+                # - The number of replicas doesn't seem to have much of an
+                #   effect.
+                # for batch_size in [20, 30, 40, 50, 60, 70, 80, 90, 100]
+                # for (
+                #     num_batchers,            # 0
+                #     num_proxy_leaders,       # 1
+                #     flexible,                # 2
+                #     num_acceptor_groups,     # 3
+                #     num_acceptors_per_group, # 4
+                #     num_replicas,            # 5
+                #     num_proxy_replicas,      # 6
+                #     batch_flush,             # 7
+                #     batch_size,              # 8
+                #     num_client_procs,        # 9
+                #     num_clients_per_proc,    # 10
+                # ) in [
+                #     # 0  1      2  3  4  5  6     7           8   9   10
+                #     ( 2, 2, False, 1, 3, 2, 2, True, batch_size, 40, 100),
+                #     ( 2, 2, False, 1, 3, 2, 3, True, batch_size, 40, 100),
+                #     ( 2, 2, False, 1, 3, 2, 4, True, batch_size, 40, 100),
+                #     ( 2, 2, False, 1, 3, 2, 5, True, batch_size, 40, 100),
+                #     ( 2, 2, False, 1, 3, 2, 6, True, batch_size, 40, 100),
+                #     ( 2, 2, False, 1, 3, 2, 7, True, batch_size, 40, 100),
+                #     ( 2, 2, False, 1, 3, 2, 8, True, batch_size, 40, 100),
+                # ]
+
+                for (
+                    num_batchers,            # 0
+                    num_proxy_leaders,       # 1
+                    flexible,                # 2
+                    num_acceptor_groups,     # 3
+                    num_acceptors_per_group, # 4
+                    num_replicas,            # 5
+                    num_proxy_replicas,      # 6
+                    batch_flush,             # 7
+                    batch_size,              # 8
+                    num_client_procs,        # 9
+                    num_clients_per_proc,    # 10
+                ) in [
+                    # 0  1      2  3  4  5  6     7    8   9   10
+                    ( 2, 2, False, 1, 3, 2, 3, True,   1,  1,   1),
+                    ( 2, 2, False, 1, 3, 2, 3, True,  10,  1,  50),
+                    ( 2, 2, False, 1, 3, 2, 3, True,  20,  1, 100),
+                    ( 2, 2, False, 1, 3, 2, 3, True,  40,  2, 100),
+                    ( 2, 2, False, 1, 3, 2, 3, True,  40,  3, 100),
+                    ( 2, 2, False, 1, 3, 2, 3, True, 100,  4, 100),
+                    ( 2, 2, False, 1, 3, 2, 3, True, 100,  5, 100),
+                    ( 2, 2, False, 1, 3, 2, 3, True, 100,  6, 100),
+                    ( 2, 2, False, 1, 3, 2, 3, True, 100,  7, 100),
+                    ( 2, 2, False, 1, 3, 2, 3, True, 100,  8, 100),
+                    ( 2, 2, False, 1, 3, 2, 3, True, 100,  9, 100),
+                    ( 2, 2, False, 1, 3, 2, 3, True, 100, 10, 100),
+                    ( 2, 2, False, 1, 3, 2, 3, True, 100, 12, 100),
+                    ( 2, 2, False, 1, 3, 2, 3, True, 100, 14, 100),
+                    ( 2, 2, False, 1, 3, 2, 3, True, 100, 16, 100),
+                    ( 2, 2, False, 1, 3, 2, 3, True, 100, 18, 100),
+                    ( 2, 2, False, 1, 3, 2, 3, True, 100, 20, 100),
+                    ( 2, 2, False, 1, 3, 2, 3, True, 100, 25, 100),
+                    ( 2, 2, False, 1, 3, 2, 3, True, 100, 30, 100),
+                    ( 2, 2, False, 1, 3, 2, 3, True, 100, 35, 100),
+                    ( 2, 2, False, 1, 3, 2, 3, True, 100, 40, 100),
+                ]
+            ] * 3
+
+        def summary(self, input: Input, output: Output) -> str:
+            return str({
+                'num_client_procs':
+                    input.num_client_procs,
+                'num_clients_per_proc':
+                    input.num_clients_per_proc,
+                'num_batchers':
+                    input.num_batchers,
+                'num_proxy_leaders':
+                    input.num_proxy_leaders,
+                'num_acceptor_groups':
+                    input.num_acceptor_groups,
+                'num_acceptors_per_group':
+                    input.num_acceptors_per_group,
+                'num_replicas':
+                    input.num_replicas,
+                'num_proxy_replicas':
+                    input.num_proxy_replicas,
+                'batch_flush':
+                    input.proxy_replica_options.batch_flush,
+                'batch_size':
+                    input.batcher_options.batch_size,
+                'write.latency.median_ms': \
+                    f'{output.write_output.latency.median_ms:.6}',
+                'write.start_throughput_1s.p90': \
+                    f'{output.write_output.start_throughput_1s.p90:.8}',
+            })
+
+    suite = Suite()
+    with benchmark.SuiteDirectory(args.suite_directory,
+                                  'batched_compartmentalized_lt') as dir:
+        suite.run_suite(dir)
+
+
+if __name__ == '__main__':
+    main(get_parser().parse_args())
